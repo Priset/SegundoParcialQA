02b4a9f0fd6f5b62dd1cb257a7dec9a5cba28735
Priset <emanuel.1kll@gmail.com>
2023-10-18 18:58:33 -0400

    Commit-Final

--- src/test/java/web/webTest/filaA/Ejercicio3Test.java
+++ src/test/java/web/webTest/filaA/Ejercicio3Test.java
@@ -1,5 +1,6 @@
 package web.webTest.filaA;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import web.webTest.TestBase;
 
@@ -12,8 +13,19 @@ public class Ejercicio3Test extends TestBase {
     private static Random rand = new Random();
 
     @Test
-    public void verifyEjercicio3Test() {
+    public void verifyEjercicio3Test() throws InterruptedException {
         mainPage.signUpButton.click();
+        signUpPage.fullNameTextBox.setText("GhostQA"+rand.nextInt(10000));
+        signUpPage.emailTextBox.setText("ghost"+rand.nextInt(10000)+"@gmail.com");
+        signUpPage.passwordTextBox.setText("ghost753159");
+        signUpPage.acceptTermsButton.click();
+        signUpPage.signUpButton.click();
+        Assertions.assertTrue(centralSection.openSettingsButton.isControlDisplayed(), "ERROR! El Usuario no se creo correctamente");
+        projectSection.addProjectButton.click();
+        projectSection.projectNameTextBox.setText(nameProject);
+        projectSection.projectNameButton.click();
+        Assertions.assertTrue(projectSection.addNameToProjectLabel(nameProject).isControlDisplayed(), "ERROR! El Proyecto no se creo correctamente");
+        Thread.sleep(3000);
 
     }
 }
